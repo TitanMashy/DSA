5eecb0ea446c1464de6373adf145729987562355
TitanMashy <titanmashyxd@gmail.com>
2025-10-14 21:12:57 +0530

    test commit

--- test.cpp
+++ test.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {2, 4, 6, 8};
-    v.insert(v.begin(), 1);
-    for(auto a: v){
-        cout<<a<<" ";
-    }
+    cout<<"hello world";
     return 0;
 } 
